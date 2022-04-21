b74ff25ff3d1f099a39f48f691aee3b07a8e17eb
Lee Soyeon <leesoyena@gmail.com>
2022-04-21 16:31:30 +0900

    웹 프로그래밍 4주차 실습

--- js/intro.js
+++ js/intro.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector("#login-form")
+const idInput = document.querySelector("#login-form #id")
+const passwordInput = document.querySelector("#login-form #password")
+const button = document.querySelector("#button")
 
+function onLoginButtonClick(event) {
+    event.preventDefault()
+
+    const id = idInput.value
+    const password = passwordInput.value
+
+    console.log(id)
+    console.log(password)
+
+    if (id == "soyena729" && password == "abc0518") {
+        alert("로그인 성공!!!!")
+        window.location.href = "index.html"
+    } else {
+        alert("로그인 실패....")
+    }
+
+}
+
+button.addEventListener("click", onLoginButtonClick)
